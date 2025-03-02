860f96c1c48e2f932f4a80084978e1f16f295833
Serge <sergiusz.kozubek@gmail.com>
2025-03-02 16:39:07 +0100

    ASTRO update to the lates version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
